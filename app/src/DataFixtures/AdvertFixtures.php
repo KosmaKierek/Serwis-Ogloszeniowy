29b1699075e4fec4611a50c096b1f7bb6ab8a10e
KosmaKierek <kosma.kierek@student.uj.edu.pl>
2024-05-31 22:21:04 +0200

    relations

--- app/src/DataFixtures/AdvertFixtures.php
+++ app/src/DataFixtures/AdvertFixtures.php
@@ -5,32 +5,64 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
+use App\Entity\Enum\AdvertStatus;
+use App\Entity\Tag;
 use App\Entity\Advert;
-use DateTimeImmutable;
-use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
 /**
  * Class AdvertFixtures.
  */
-class AdvertFixtures extends AbstractBaseFixtures
+class AdvertFixtures extends AbstractBaseFixtures implements DependentFixtureInterface
 {
     /**
      * Load data.
+     *
+     * @psalm-suppress PossiblyNullPropertyFetch
+     * @psalm-suppress PossiblyNullReference
+     * @psalm-suppress UnusedClosureParam
      */
     public function loadData(): void
     {
-        for ($i = 0; $i < 10; ++$i) {
+        if (null === $this->manager || null === $this->faker) {
+            return;
+        }
+
+        $this->createMany(100, 'adverts', function (int $i) {
             $advert = new Advert();
             $advert->setTitle($this->faker->sentence);
             $advert->setCreatedAt(
-                DateTimeImmutable::createFromMutable($this->faker->dateTimeBetween('-100 days', '-1 days'))
+                \DateTimeImmutable::createFromMutable(
+                    $this->faker->dateTimeBetween('-100 days', '-1 days')
+                )
             );
             $advert->setUpdatedAt(
-                DateTimeImmutable::createFromMutable($this->faker->dateTimeBetween('-100 days', '-1 days'))
+                \DateTimeImmutable::createFromMutable(
+                    $this->faker->dateTimeBetween('-100 days', '-1 days')
+                )
             );
-            $this->manager->persist($advert);
-        }
+            /** @var Category $category */
+            $category = $this->getRandomReference('categories');
+            $advert->setCategory($category);
+
+            return $advert;
+        });
 
         $this->manager->flush();
+    }
+
+    /**
+     * This method must return an array of fixtures classes
+     * on which the implementing class depends on.
+     *
+     * @return string[] of dependencies
+     *
+     * @psalm-return array{0: CategoryFixtures::class}
+     */
+    public function getDependencies(): array
+    {
+        return [CategoryFixtures::class];
     }
 }
